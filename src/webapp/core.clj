81221abb3d10ff5f3602e28b9f396c13e0893785
Joost Diepenmaat <joost@zeekat.nl>
2016-06-01 11:26:20 +0200

    multiple pages

--- src/webapp/core.clj
+++ src/webapp/core.clj
@@ -1,11 +1,11 @@
 (ns webapp.core
-  (:require [hiccup.page :refer [html5 include-css]]
+  (:require [hiccup.page :as page :refer [include-css]]
             [hiccup.core :refer [h]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defn html-body
   [title & contents]
-  (html5
+  (page/html5
    [:head
     [:title (h title)]
     (include-css "https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic")
@@ -19,11 +19,26 @@
    :headers {"Content-Type" "text/html; charset=utf-8"}
    :body (apply html-body title contents)})
 
-(defn handler
+;;; /byebye.html
+
+(defn byebye
   [request]
   (page "A webapp"
-        [:h1 "Hello"]
-        [:p "This is the standard response page"]))
+        [:h1 "Goodbye!"]
+        [:p "Byebye"]
+        [:p [:a {:href "/"} "take me back"]]))
+
+;; :uri "/byebye.html"
+
+(defn handler
+  [request]
+  (if (= (:uri request) "/byebye")
+    (byebye request)
+    (page "A webapp"
+          (prn request)
+          [:h1 "Hello!"]
+          [:p "This is the standard response page"]
+          [:p [:a {:href "/byebye"} "Bye!"]])))
 
 (def handler-with-middleware
   (-> handler
